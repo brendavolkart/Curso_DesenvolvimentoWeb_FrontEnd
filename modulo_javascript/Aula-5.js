485f7649fb22c211c4a8f453ed21d1c61cce0f1d
brendavolkart <volkarttbrenda@gmail.com>
2024-07-17 00:14:04 -0300

    aula06

--- modulo_javascript/Aula-5.js
+++ modulo_javascript/Aula-5.js
@@ -4,3 +4,5 @@ while(idade < 18){
     console.log('PASSOU', idade);
     idade = idade + 1; 
 }
+
+//Primeiro verifica a condição depois executa 
---
+++ modulo_javascript/Aula-6.js
@@ -0,0 +1,7 @@
+let idade = 18; 
+
+do{
+    console.log('PASSOU', idade);
+}while(idade < 18);
+
+//Primeiro executa e depois verifica a condição
