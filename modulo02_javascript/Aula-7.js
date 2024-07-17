80a8139ca5f6f131ee9a8ed7f58ee897c73def2e
brendavolkart <volkarttbrenda@gmail.com>
2024-07-17 19:13:43 -0300

    Aula07

---
+++ modulo02_javascript/Aula-7.js
@@ -0,0 +1,10 @@
+
+const idades = [18, 23, 55, 92, 143]
+
+for (let i = 0; i < idades.length; i++){
+
+    console.log('resultado', idades[i]);
+
+}
+
+//Array de objetos é uma ferramenta usada para armazenar vários valores
