cd3e8501a3e5fb1765671bedaf366b79a00e8980
brendavolkart <volkarttbrenda@gmail.com>
2024-07-17 19:23:20 -0300

    Aula08

---
+++ modulo02_javascript/Aula-08.js
@@ -0,0 +1,8 @@
+const salario = '103';
+const salarioNumero = Number(salario);
+
+console.log('salario: ', salario)
+console.log('salarioNumero: ' , salarioNumero)
+console.log('salario numero: ', salarioNumero.toFixed(2))
+
+//Conversão de string para número
