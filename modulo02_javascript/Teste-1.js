ae824186d4af914b7fe3d4fe10d270ba11afe024
brendavolkart <volkarttbrenda@gmail.com>
2024-07-17 21:49:51 -0300

    Desafio 1

---
+++ modulo02_javascript/Teste-1.js
@@ -0,0 +1,14 @@
+//Calcular e mostrar na tela a quantidade de litros de combustível gastos em uma viagem 
+
+const kmLitro = 12;
+
+function calcularDist(velMed, tempo){
+    return distancia = velMed * tempo 
+
+    }
+
+function resultCombus(distancia, litroCombus){
+    return total = distancia * litroCombus
+}
+
+console.log('Serão necessários', resultCombus(calcularDist(5,5),kmLitro).toFixed(3) , 'litros de combustível para viajar')
