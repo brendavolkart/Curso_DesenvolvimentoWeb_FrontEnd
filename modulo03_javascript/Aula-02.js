c092ecbce8992aa223140153c56af1a0d1b99db7
brendavolkart <volkarttbrenda@gmail.com>
2024-07-22 14:53:26 -0300

    Aula02

---
+++ modulo03_javascript/Aula-02.js
@@ -0,0 +1,15 @@
+const idade = 654;
+
+const exibirIdade = () => {
+    const idade = 23; 
+    console.log(idade)
+}
+
+const exibirExibirIdade = () => {
+    exibirIdade();
+    console.log(idade);
+}
+
+exibirExibirIdade();
+
+//Aula sobre escopo léxico!
