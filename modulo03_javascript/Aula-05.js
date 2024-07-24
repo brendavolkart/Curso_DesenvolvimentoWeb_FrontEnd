30827204836a15e518786616b160a3b7e7501fff
brendavolkart <volkarttbrenda@gmail.com>
2024-07-23 22:14:22 -0300

    Aula05

---
+++ modulo03_javascript/Aula-05.js
@@ -0,0 +1,14 @@
+//Função recursiva é uma função que chama ela mesma até que se cumpra uma condição.
+
+const fatorial = (valor) => {
+    if (valor === 0 || valor === 1 ){
+        return 1;    
+    }
+    return valor * fatorial(valor - 1);
+    // 4 * fatorial 3
+    // 3 * fatorial 2
+    // 2 * fatorial 1
+    // 1 
+}
+
+console.log(fatorial(4));
