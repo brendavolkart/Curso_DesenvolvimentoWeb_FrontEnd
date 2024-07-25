7ee8ae5ea4d658d34fe361e6aeff2c9019edf5a3
brendavolkart <volkarttbrenda@gmail.com>
2024-07-25 13:17:21 -0300

    Aula04

---
+++ modulo04_javascript/Aula-04.js
@@ -0,0 +1,34 @@
+//USANDO O ARRAY FIND 
+
+//FIND PROCURA UM OBJETO COM AQUELA CONDIÇÃO E O PRIMEIRO QUE ELE ACHAR ELE DEVOLVE PARA UM OBJETO
+
+const lista = [
+    {
+        nome: 'José',
+        idade: 13
+    },
+    {
+        nome: 'João',
+        idade: 17
+    },
+    {
+        nome: 'Fernanda',
+        idade: 8
+    },
+    {
+        nome: 'Maria',
+        idade: 7
+    },
+    {
+        nome: 'Sandra',
+        idade: 90
+    },
+    {
+        nome: 'Pedro',
+        idade: 25
+    },
+]
+
+const novaPessoa = lista.find((pessoa) => pessoa.nome === 'Pedro');
+
+console.log(novaPessoa);
